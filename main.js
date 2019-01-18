22aeec56cb18c24ccba30e2e2c1f42f630daebc7
Alex Rolfe <alex.rolfe27@gmail.com>
2019-01-18 06:50:21 -0600

    Adds string formatting to confirm to use of '+'s in google api docs

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const BOOKS_KEY = "AIzaSyB9a6wUkQFgzuSu_4rEr0wX2-UL3GQgxB4";
 const baseURL = "https://www.googleapis.com/books/v1/";
 
 function search() {
-    const searchString = document.querySelector('input').value;
-    console.log(searchString);
+    const searchText = document.querySelector('input').value;
+    fetchVolumes(searchText);
+}
+
+function fetchVolumes(searchText) {
+    fetch(constructVolumesRequest(searchText))
+        .then(r => r.json())
+        .then(d => console.log(d));
 }
 
 function constructVolumesRequest(queryString) {
-    return baseURL + "volumes?q=" + queryString;
+    const formattedString = queryString.split(" ").join("+");
+    return baseURL + "volumes?q=" + formattedString;
 }
-
-fetch(constructVolumesRequest("john+mills"))
-    .then(r => r.json())
-    .then(d => console.log(d));
 
