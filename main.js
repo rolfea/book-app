ea3de7e948816aac5824899072655d7aa5efb799
Alex Rolfe <alex.rolfe27@gmail.com>
2019-01-18 07:18:31 -0600

    Adds simple DOM manipulation to pull in book info - need to fix data passed through so I have access to publishing company

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 const BOOKS_KEY = "AIzaSyB9a6wUkQFgzuSu_4rEr0wX2-UL3GQgxB4";
 const baseURL = "https://www.googleapis.com/books/v1/";
 
-function search() {
-    const searchText = document.querySelector('input').value;
-    fetchVolumes(searchText);
+function displaySearchResults(searchResults) {
+    const listParent = document.querySelector('#search-results');            
+
+    searchResults.forEach(sr => {
+        const li = document.createElement("li");
+        li.innerText = `Author: ${sr.authors} Title: ${sr.title} Publishing Company: ${sr.publisher} Image:`
+        listParent.appendChild(li);
+    })
 }
 
 function trimData(data) {
-    console.log(data);
-    return data.map( b => b.volumeInfo);
-    
-}
-
-function fetchVolumes(searchText) {
-    fetch(constructVolumesRequest(searchText))
-        .then(r => r.json())
-        .then(d => console.log(trimData(d.items)));
+    return data.map( b => b.volumeInfo);    
 }
 
 function constructVolumesRequest(queryString) {
     const formattedString = queryString.split(" ").join("+");
     return baseURL + "volumes?q=" + formattedString;
+}
+
+function fetchVolumes(searchText) {
+    fetch(constructVolumesRequest(searchText))
+        .then(r => r.json())
+        .then(d => trimData(d.items))
+        .then(td => displaySearchResults(td));
+}
+
+function search() {
+    const searchText = document.querySelector('input').value;
+    fetchVolumes(searchText);
 }
 
